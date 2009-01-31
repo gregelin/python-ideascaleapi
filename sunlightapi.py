f1af2b2c15f812acff95e19fc2f58a5b0c5d2c54
James Turk <james.p.turk@gmail.com>
2009-01-30 19:40:28 -0500

    wordlist (and removed whitespace)

--- sunlightapi.py
+++ sunlightapi.py
@@ -31,37 +31,37 @@ class Legislator(SunlightApiObject):
             fname = self.firstname
         return '%s. %s %s (%s-%s)' % (self.title, fname, self.lastname,
                                       self.party, self.state)
-    
+
 class LegislatorSearchResult(SunlightApiObject):
     def __init__(self, d):
         self.legislator = Legislator(d['legislator'])
         self.score = d['score']
-        
+
     def __str__(self):
         return '%s %s' % (self.score, self.legislator)
 
 class District(SunlightApiObject):
     def __str__(self):
         return '%s-%s' % (self.state, self.number)
-    
+
 class Lobbyist(SunlightApiObject):
     def __str__(self):
         return '%s %s' % (self.firstname, self.lastname)
 
-class Issue(SunlightApiObject):    
+class Issue(SunlightApiObject):
     def __str__(self):
         return '%s (%s)' % (self.code, self.specific_issue)
-    
+
 class Filing(SunlightApiObject):
     def __init__(self, d):
         self.__dict__ = d
         self.lobbyists = [Lobbyist(l['lobbyist']) for l in self.lobbyists]
         self.issues = [Issue(i['issue']) for i in self.issues]
-        
+
     def __str__(self):
         return '%s - %s for %s' % (self.filing_id, self.registrant_name,
                                    self.client_name)
-    
+
 class FilingSearchResult(SunlightApiObject):
     def __init__(self, d):
         self.__dict__ = d['lobbyist']
@@ -74,14 +74,14 @@ class FilingSearchResult(SunlightApiObject):
 # namespaces #
 
 class sunlight(object):
-    
+
     apikey = None
-    
+
     @staticmethod
     def _apicall(func, params):
         if sunlight.apikey is None:
             raise SunlightApiError('Missing sunlight apikey')
-        
+
         url = 'http://services.sunlightlabs.com/api/%s.json?apikey=%s&%s' % \
               (func, sunlight.apikey, urllib.urlencode(params))
         try:
@@ -97,52 +97,52 @@ class sunlight(object):
         def get(**kwargs):
             result = sunlight._apicall('legislators.get', kwargs)['legislator']
             return Legislator(result)
-        
+
         @staticmethod
         def getList(**kwargs):
             results = sunlight._apicall('legislators.getList', kwargs)
             return [Legislator(l['legislator']) for l in results['legislators']]
-        
+
         @staticmethod
         def search(name, threshold=0.9):
             params =  {'name':name, 'threshold': threshold}
             results = sunlight._apicall('legislators.search', params)['results']
             return [LegislatorSearchResult(r['result']) for r in results]
-    
+
         @staticmethod
         def allForZip(zipcode):
             results = sunlight._apicall('legislators.allForZip', {'zip':zipcode})
             return [Legislator(l['legislator']) for l in results['legislators']]
-        
+
     class districts(object):
         @staticmethod
         def getDistrictsFromZip(zipcode):
             results = sunlight._apicall('districts.getDistrictsFromZip', {'zip':zipcode})
             return [District(r['district']) for r in results['districts']]
-        
+
         @staticmethod
         def getZipsFromDistrict(state, district):
             params = {'state':state, 'district':district}
             results = sunlight._apicall('districts.getZipsFromDistrict', params)
             return results['zips']
-        
+
         @staticmethod
         def getDistrictFromLatLong(latitude, longitude):
             params = {'latitude':latitude, 'longitude':longitude}
             result = sunlight._apicall('districts.getDistrictFromLatLong', params)
             return District(result['districts'][0]['district'])
-        
+
     class lobbyists(object):
         @staticmethod
         def getFiling(id):
             result = sunlight._apicall('lobbyists.getFiling', {'id':id})
             return Filing(result['filing'])
-            
+
         @staticmethod
         def getFilingList(**kwargs):
             results = sunlight._apicall('lobbyists.getFilingList', kwargs)
             return [Filing(f['filing']) for f in results['filings']]
-        
+
         @staticmethod
         def search(name, year=None, threshold=0.9):
             if year == None:
@@ -151,4 +151,32 @@ class sunlight(object):
             params = {'name':name, 'year':year, 'threshold': threshold}
             results = sunlight._apicall('lobbyists.search', params)['results']
             return [FilingSearchResult(f['result']) for f in results]
-    
+
+    class wordlist(object):
+
+        @staticmethod
+        def _apicall(call, body=None):
+            base_url = 'http://services.sunlightlabs.com/api/wordlist'
+            url = '%s/%s/?apikey=%s' % (base_url, call, sunlight.apikey)
+            try:
+                if body:
+                    data = urllib.urlencode(body)
+                else:
+                    data = None
+                return urllib2.urlopen(url, data).read()
+            except urllib2.HTTPError, e:
+                raise SunlightApiError(e.read())
+
+        @staticmethod
+        def get(list_name):
+            words = sunlight.wordlist._apicall(list_name)
+            return words.split('~')
+
+        @staticmethod
+        def update(list_name, words):
+            sunlight.wordlist._apicall(list_name, {'words': '\n'.join(words)})
+
+        @staticmethod
+        def filter_stopwords(list_name, text):
+            call = '%s/filter_stopwords' % list_name
+            return sunlight.wordlist._apicall(call, {'text': text})
