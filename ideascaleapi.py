f63468517248ae6787396ebdce82c7db5a47769c
Greg Elin <gregelin@Greg-17inch.local>
2009-05-30 14:11:43 -0400

    Created first working methods classes for ideascale

--- ideascaleapi.py
+++ ideascaleapi.py
@@ -1,8 +1,25 @@
-""" Python library for interacting with the Sunlight Labs API.
+""" Python library for interacting with the IdeaScale's API (Alpha version).
 
     The Sunlight Labs API (http://services.sunlightlabs.com/api/) provides basic
     legislator data, conversion between ids, and zipcode-district lookups.
     
+
+    Current method calls available:
+    Idea related Methods:
+        ideascale.ideas.getTopIdeas
+        ideascale.ideas.getRecentIdeas
+    
+    Tag Related Methods:
+        ideascale.tags.getTopTags
+
+    User Related Methods:
+        ideascale.users.getTopUsers
+
+    
+    
+    References
+    http://opengov.ideascale.com/akira/ideascaleStatic.do?mode=api
+    http://ideas.ideascale.com/akira/ideafactory.do?discussionID=12038
     
     Changes from original python-sunlightlabs
     
@@ -10,6 +27,8 @@
         - Changed SunlightApiObject to ApiObject
         - Changed SunlightApiError to ApiError
         - Changed class 'sunlight' to 'ideascale'
+        - Added some simple test methods
+        - Repointed url template to ideascale url instead of Sunlight Labs
 """
 
 __author__ = "Greg Elin (greg@greg@fotonotes.net); James Turk (jturk@sunlightfoundation.com)"
@@ -31,23 +50,10 @@ class ApiObject(object):
     def __init__(self, d):
         self.__dict__ = d
 
-class getTopIdeas(ApiObject):
+class TopIdeas(ApiObject):
     def __str__(self):
-        if self.nickname:
-            fname = self.nickname
-        else:
-            fname = self.firstname
-        return '%s. %s %s (%s-%s)' % (self.title, fname, self.lastname,
-                                      self.party, self.state)
-
-class Legislator(ApiObject):
-    def __str__(self):
-        if self.nickname:
-            fname = self.nickname
-        else:
-            fname = self.firstname
-        return '%s. %s %s (%s-%s)' % (self.title, fname, self.lastname,
-                                      self.party, self.state)
+        return [idea for idea in self.ideas]
+#        return '%s. %s %s (%s-%s)' % (self.title, fname, self.lastname, self.party, self.state)
 
 
 # namespaces #
@@ -57,11 +63,11 @@ class ideascale(object):
     apikey = None
 
     @staticmethod
-    def _apicall(func, params):
-        if sunlight.apikey is None:
+    def _apicall(func, params={}):
+        if ideascale.apikey is None:
             raise ApiError('Missing ideascale apikey')
 
-        url = 'http://api.ideascale.com/akira/api/ideascale%s?apiKey=%s&%s' % \
+        url = 'http://api.ideascale.com/akira/api/ideascale.%s?apiKey=%s&%s' % \
               (func, ideascale.apikey, urllib.urlencode(params))
         try:
             response = urllib2.urlopen(url).read()
@@ -70,54 +76,39 @@ class ideascale(object):
             raise ApiError(e.read())
         except (ValueError, KeyError), e:
             raise ApiError('Invalid Response')
-
-    class getTopIdeas(object):
-        @staticmethod
-        def get(**kwargs):
-            result = sunlight._apicall('legislators.get', kwargs)['legislator']
-            return Legislator(result)
-
-        @staticmethod
-        def getList(**kwargs):
-            results = sunlight._apicall('legislators.getList', kwargs)
-            return [Legislator(l['legislator']) for l in results['legislators']]
-
-        @staticmethod
-        def search(name, threshold=0.9, all_legislators=False):
-            params =  {'name':name, 'threshold': threshold}
-            if all_legislators:
-                params['all_legislators'] = 1
-            results = sunlight._apicall('legislators.search', params)['results']
-            return [LegislatorSearchResult(r['result']) for r in results]
-
-        @staticmethod
-        def allForZip(zipcode):
-            results = sunlight._apicall('legislators.allForZip', {'zip':zipcode})
-            return [Legislator(l['legislator']) for l in results['legislators']]
-
     
-    class legislators(object):
+    @staticmethod
+    def _apicall_url(func, params={}):
+        if ideascale.apikey is None:
+            raise ApiError('Missing ideascale apikey')
+
+        url = 'http://api.ideascale.com/akira/api/ideascale.%s?apiKey=%s&%s' % \
+              (func, ideascale.apikey, urllib.urlencode(params))
+        return url
+    
+    @staticmethod
+    def test():
+        return "Test OK"
+        
+    @staticmethod
+    def get_apikey():
+        if ideascale.apikey is None:
+            raise ApiError('Missing ideascale apikey')
+        else:
+            return ideascale.apikey
+        
+    class topIdeas(object):
         @staticmethod
         def get(**kwargs):
-            result = sunlight._apicall('legislators.get', kwargs)['legislator']
-            return Legislator(result)
+            result = ideascale._apicall('getTopIdeas', kwargs)
+            return [TopIdeas(response['ideas']) for response in results]
 
+
+    class recentIdeas(object):
         @staticmethod
-        def getList(**kwargs):
-            results = sunlight._apicall('legislators.getList', kwargs)
-            return [Legislator(l['legislator']) for l in results['legislators']]
-
-        @staticmethod
-        def search(name, threshold=0.9, all_legislators=False):
-            params =  {'name':name, 'threshold': threshold}
-            if all_legislators:
-                params['all_legislators'] = 1
-            results = sunlight._apicall('legislators.search', params)['results']
-            return [LegislatorSearchResult(r['result']) for r in results]
-
-        @staticmethod
-        def allForZip(zipcode):
-            results = sunlight._apicall('legislators.allForZip', {'zip':zipcode})
-            return [Legislator(l['legislator']) for l in results['legislators']]
+        def get(**kwargs):
+            result = ideascale._apicall('ideas.getRecentIdeas', kwargs)
+            return [response['ideas'] for response in results]
 
 
+ 
